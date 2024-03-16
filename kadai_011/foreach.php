a8ca78bf6aa9b8938f1a46f0962af60be23fcfeb
mpanda0301 <161836059+mpanda0301@users.noreply.github.com>
2024-03-16 21:41:23 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>kadai_011</title>
+</head>
+<body>
+  <p>
+    <?php
+      $array = ['名前' => '玉ねぎ', 
+          '値段' => 200,
+          '産地' => '北海道'];
+      foreach ($array as $key => $value) {
+        echo "{$key}：{$value}<br>";
+       }
+    ?>
+  </p>
+  
+</body>
+</html>
